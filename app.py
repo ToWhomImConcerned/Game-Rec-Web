bc1791ef831d91b10d0e9e78c024dc53683a0ada
ToWhomImConcerned <Cburket129@gmail.com>
2026-04-18 23:04:21 -0700

    fix bugs

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 import requests as req
-from openai import OpenAI
 
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 api_key = os.getenv("RAWG_KEY")
-print("API KEY", api_key)
 
 app = Flask(__name__)
 
